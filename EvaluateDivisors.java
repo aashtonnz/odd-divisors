a49d21513462ecd7d8b7cbdbf6c22faa8578dff0
aashtonnz <aashtonnz@gmail.com>
2015-05-16 17:12:35 +1200

    Update EvaluateDivisors.java

--- EvaluateDivisors.java
+++ EvaluateDivisors.java
@@ -47,16 +47,20 @@ public class EvaluateDivisors {
      * @return the solution
      */
     private static int solution(long a, long b, int k) {
+        // If K is prime there is a more efficient algorithm.
+        if (prime(k)) {
+            return primeSolution(a, b, k);
+        }
         int result = 0;
 
         /* Integers with an odd number of divisors are perfect squares, since
            every divisor n of c corresponds to a pair (n, c/n) unless n = c/n,
-           that is, c = n*n. */
+           that is, c = n^2. */
         for (long n = (long) Math.sqrt(a); n*n <= b; n++) {
-            // 1, n, and n*n are divisors of n*n.
+            // 1, n, and n^2 are divisors of n^2.
             int divisors = 3;
 
-            /* Count other divisors in pairs (m, n*n/m). If m > n then n*n/m < n
+            /* Count other divisors in pairs (m, n^2/m). If m > n then n^2/m < n
                and so the pair has been counted. Therefore stop once m < n. */
             for (long m = 2; m < n && divisors <= k; m++) {
                 if (n*n % m == 0) {
@@ -68,6 +72,50 @@ public class EvaluateDivisors {
             }
         }
         return result;
+    }
+    
+    /**
+     * Returns the solution provided K is prime.
+     * 
+     * @param a A
+     * @param b B
+     * @param k K
+     * 
+     * @return the solution
+     */
+    private static int primeSolution(long a, long b, int k) {
+        int result = 0;
+        
+        /* If K is prime and n has K divisors then n = p^(K - 1) for some prime p
+           (see README.txt for details). */
+        int n = 2;
+        while (Math.pow(n, k - 1) < a) {
+            n++;
+        }
+        while (Math.pow(n, k - 1) <= b) {
+            if (prime(n++)) {
+                result++;
+            }
+        }
+        return result;
+    }
+    
+    /**
+     * Returns true if the given positive integer is prime.
+     * 
+     * @param n a positive integer
+     * 
+     * @return true if n is prime
+     */
+    private static boolean prime(int n) {
+        /* If m > sqrt(n) divides n then some integer less than m divides
+           n. Therefore the loops stops when m > sqrt(n). */
+        for (int m = 2; m <= Math.sqrt(n); m++) {
+            if (n % m == 0) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
